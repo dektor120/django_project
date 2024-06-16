18e8598a1ab66df65408986c722d61afc3ca729d
Dexter <litva3804@mail.ru>
2024-06-17 05:14:10 +0700

    Updating forms, views

--- university/forms.py
+++ university/forms.py
@@ -15,3 +15,18 @@ class GradeForm(forms.ModelForm):
     class Meta:
         model = Grade
         fields = ['student', 'subject', 'grade']
+
+
+class GradeFilterForm(forms.Form):
+    student = forms.ModelChoiceField(
+        queryset=Student.objects.all(),
+        label='Студент',
+        required=False,
+        empty_label="Все студенты"
+    )
+    subject = forms.ModelChoiceField(
+        queryset=Subject.objects.all(),
+        label='Предмет',
+        required=False,
+        empty_label="Все предметы"
+    )
--- university/views.py
+++ university/views.py
@@ -1,55 +1,155 @@
-from django.shortcuts import render, redirect
-from .models import Student, Subject, Grade
+from django.shortcuts import render, redirect, get_object_or_404
 from django.db.models import Avg
-from django.http import Http404
+from .models import Student, Subject, Grade
+from .forms import StudentForm, SubjectForm, GradeForm
+
+
 
 def index(request):
     return render(request, 'university/index.html')
 
-def students(request):
+
+def student_list(request):
     students = Student.objects.all()
-    return render(request, 'university/students.html', {'students': students})
+    return render(request, 'university/student_list.html', {'students': students})
 
-def student_detail(request, student_id):
-    try:
-        student = Student.objects.get(pk=student_id)
-    except Student.DoesNotExist:
-        raise Http404("Student does not exist")
-    grades = Grade.objects.filter(student=student)
-    context = {
-        'student': student,
-        'grades': grades,
-    }
-    return render(request, 'university/student_detail.html', context)
 
-def subjects(request):
+def student_create(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'university/student_form.html', {'form': form})
+
+
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'university/student_form.html', {'form': form})
+
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        student.delete()
+        return redirect('student_list')
+    return render(request, 'university/student_confirm_delete.html', {'student': student})
+
+
+def subject_list(request):
     subjects = Subject.objects.all()
-    return render(request, 'university/subjects.html', {'subjects': subjects})
+    return render(request, 'university/subject_list.html', {'subjects': subjects})
 
-def subject_detail(request, subject_id):
-    try:
-        subject = Subject.objects.get(pk=subject_id)
-    except Subject.DoesNotExist:
-        raise Http404("Subject does not exist")
-    grades = Grade.objects.filter(subject=subject)
-    context = {
-        'subject': subject,
-        'grades': grades,
-    }
-    return render(request, 'university/subject_detail.html', context)
 
-def grades(request):
+def subject_create(request):
+    if request.method == 'POST':
+        form = SubjectForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('subject_list')
+    else:
+        form = SubjectForm()
+    return render(request, 'university/subject_form.html', {'form': form})
+
+
+def subject_update(request, pk):
+    subject = get_object_or_404(Subject, pk=pk)
+    if request.method == 'POST':
+        form = SubjectForm(request.POST, instance=subject)
+        if form.is_valid():
+            form.save()
+            return redirect('subject_list')
+    else:
+        form = SubjectForm(instance=subject)
+    return render(request, 'university/subject_form.html', {'form': form})
+
+
+def subject_delete(request, pk):
+    subject = get_object_or_404(Subject, pk=pk)
+    if request.method == 'POST':
+        subject.delete()
+        return redirect('subject_list')
+    return render(request, 'university/subject_confirm_delete.html', {'subject': subject})
+
+
+def grade_list(request):
     grades = Grade.objects.all()
-    return render(request, 'university/grades.html', {'grades': grades})
+    form = GradeFilterForm(request.GET)
+
+    if form.is_valid():
+        student = form.cleaned_data.get('student')
+        subject = form.cleaned_data.get('subject')
+
+        if student:
+            grades = grades.filter(student=student)
+        if subject:
+            grades = grades.filter(subject=subject)
+
+    order_by = request.GET.get('order_by', '')
+    if order_by in ('student__name', '-student__name', 'subject__name', '-subject__name'):
+        grades = grades.order_by(order_by)
+
+    context = {
+        'grades': grades,
+        'form': form,
+    }
+    return render(request, 'university/grade_list.html', context)
+
+
+def grade_create(request):
+    if request.method == 'POST':
+        form = GradeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('grade_list')
+    else:
+        form = GradeForm()
+    return render(request, 'university/grade_form.html', {'form': form})
+
+
+def grade_update(request, pk):
+    grade = get_object_or_404(Grade, pk=pk)
+    if request.method == 'POST':
+        form = GradeForm(request.POST, instance=grade)
+        if form.is_valid():
+            form.save()
+            return redirect('grade_list')
+    else:
+        form = GradeForm(instance=grade)
+    return render(request, 'university/grade_form.html', {'form': form})
+
+
+def grade_delete(request, pk):
+    grade = get_object_or_404(Grade, pk=pk)
+    if request.method == 'POST':
+        grade.delete()
+        return redirect('grade_list')
+    return render(request, 'university/grade_confirm_delete.html', {'grade': grade})
+
 
 def best_students(request):
-    students_avg = Grade.objects.values('student__name').annotate(average_grade=Avg('grade')).order_by('-average_grade')[:3]
+    students_avg = Grade.objects.values('student__name').annotate(
+        average_grade=Avg('grade')
+    ).filter(average_grade__gt=3.5).order_by('-average_grade')
     return render(request, 'university/best_students.html', {'students_avg': students_avg})
 
+
 def worst_students(request):
-    students_avg = Grade.objects.values('student__name').annotate(average_grade=Avg('grade')).order_by('average_grade')[:3]
+    students_avg = Grade.objects.values('student__name').annotate(
+        average_grade=Avg('grade')
+    ).filter(average_grade__lte=3.5).order_by('average_grade')
     return render(request, 'university/worst_students.html', {'students_avg': students_avg})
 
 def subject_average(request):
-    subjects_avg = Grade.objects.values('subject__name').annotate(average_grade=Avg('grade')).order_by('subject__name')
+    subjects_avg = Grade.objects.values('subject__name').annotate(
+        average_grade=Avg('grade')).order_by('subject__name')
     return render(request, 'university/subject_average.html', {'subjects_avg': subjects_avg})
